7c4df4388981a1c16e2ca4a0cc28090b5b619cae
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-10 20:24:28 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+//function expression slightly different than function declaration
+const divide = function() {
+    return 2000/100;
+}
+
+//arrow function
+const square = num => {
+    return num * num; 
+}
+
+//arrow function two parameters
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
